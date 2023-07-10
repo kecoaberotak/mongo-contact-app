3173d6a0bae0c92f70f3d26a9a6c00a76afedbe8
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-10 23:32:29 +0700

    kelar fitur edit contact

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
 const {body, validationResult, check} = require('express-validator');
+const methodOverride = require('method-override');
 
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -13,6 +14,10 @@ const Contact = require('./model/contact');
 
 app = express();
 const port = 3000;
+
+// Setup Method-Override
+app.use(methodOverride('_method'));
+
 
 // Setup view engine - ejs
 app.set('view engine', 'ejs');
@@ -120,8 +125,78 @@ app.post('/contact',
 });
 
 
+// Proses Hapus Contact
+// Versi 1 - ga pake override
+// app.get('/contact/delete/:nama', async(req, res) => {
+//   const contact = await Contact.findOne({nama: req.params.nama});
+
+//   if(!contact){
+//     res.status(404);
+//     res.send('<h1>404</h1>');
+//   }else {
+//     Contact.deleteOne({_id: contact._id}).then(result => {
+//       req.flash('msg', 'Data berhasil dihapus');
+//       res.redirect('/contact');
+//     });
+//   }
+// });
+
+// Versi 2 - pake override
+app.delete('/contact', (req, res) => {
+  Contact.deleteOne({nama: req.body.nama}).then(result => {
+    req.flash('msg', 'Data berhasil dihapus');
+    res.redirect('/contact');
+  });
+});
+
+
+// Halaman Form Edit Data Contact
+app.get('/contact/edit/:nama', async(req, res) => {
+  const contact = await Contact.findOne({nama: req.params.nama})
+  res.render('edit-contact', {title : 'Form Edit Contact', layout : 'layouts/main-layout.ejs', contact});
+});
+
+
+// Proses Edit Data Contact
+app.put('/contact', 
+[
+  body('nama').custom(async (value, {req}) => {
+    const duplikat = await Contact.findOne({nama: value});
+    if(value !== req.body.oldNama && duplikat){
+      throw new Error('Nama sudah digunakan');
+    }
+    return true;
+  }),
+  check('email', 'Email Tidak Valid').isEmail(), 
+  check('nomorHp', 'Nomor HP Tidak Valid').isMobilePhone('id-ID')
+] ,(req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    res.render('edit-contact', 
+    {
+      title   : 'Form Edit Contact', 
+      layout  : 'layouts/main-layout.ejs',
+      errors  : errors.array(),
+      contact : req.body
+    });
+  } else {
+    Contact.updateOne(
+        {_id: req.body._id},
+        {
+          $set: {
+            nama: req.body.nama,
+            email: req.body.email,
+            nomorHp: req.body.nomorHp
+          }
+        }
+      ).then(result => {
+        req.flash('msg', 'Data berhasil diubah');
+        res.redirect('/contact');
+      });
+  };
+});
+
 // Halaman Detail Contact
-// Harus diletakkan paling akhir, biar route lain kebaca
 app.get('/contact/:nama', async (req, res) => {
   const contact = await Contact.findOne({nama: req.params.nama});
 
